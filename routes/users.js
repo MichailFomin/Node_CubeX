0466571c7a134b55f598cd57cb51d84084dd4cef
verako <vera.kopylchuk@gmail.com>
2018-07-29 21:16:43 +0300

    verification of emails during registration

--- routes/users.js
+++ routes/users.js
@@ -10,11 +10,11 @@ var userName=false;
 var userRole=false;
 var errorpas=false;
 var errorlog=false;
-
+var err='true';
 //registration
 router.get('/registration',function (req,res) {//маршрутизация на registration
-    req.session.user ? res.render('registration',{userName:req.session.user,userRole:req.session.role}) :
-    res.render('registration',{userName,userRole});
+    req.session.user ? res.render('registration',{userName:req.session.user,userRole:req.session.role,err}) :
+    res.render('registration',{userName,userRole,err});
 });
 
 //login
@@ -29,16 +29,19 @@ router.post('/registration',function (req,res) { //маршрутизация н
     var email =  req.body.email;
     var password =  req.body.password;
     var password2 =  req.body.password2;
-
+   
     //validator
     req.checkBody('name', 'Name is required').notEmpty();
     req.checkBody('name', 'Name maximum length 15 characters').isLength({ max: 15 });
+    req.checkBody('name', 'Name min length 2 characters').isLength({ min: 2 });
     req.checkBody('email', 'Email is required').notEmpty();
     req.checkBody('email', 'Email is not valid').isEmail();
     req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password', 'Password maximum length 15 characters').isLength({ max: 15 });
+    req.checkBody('password', 'Password min length 3 characters').isLength({ min: 3 });
     req.checkBody('password2', 'Password do not match').equals(req.body.password);
-
     var errors=req.validationErrors();
+   
     if (errors){
         res.render('registration',{
             errors:errors,
@@ -47,13 +50,23 @@ router.post('/registration',function (req,res) { //маршрутизация н
         });
     }
     else{
+
         var hash = bcrypt.hashSync(req.body.password, 10);
 
         db.users.create({'email' : req.body.email, 'name':req.body.name, 'role':'user', 'password':hash}).then((users) => {
+            res.redirect('/login');
              
+        }).catch(function(err) {
+            if (err){
+                err='a user with this email '+req.body.email+' already registered';
+                res.render('registration',{
+                    err:err,
+                    userName,
+                    userRole
+                });
+            }
+            
         });
-       
-      res.redirect('/login');
     }
 });
 
